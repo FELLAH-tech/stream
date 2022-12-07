c056644e4a13d7571436cea19e7a7ed134cde99d
FELLAH-tech <hicham8fellah@gmail.com>
2022-12-07 23:48:26 +0100

    last

--- streaming/views/ffmpeg.sh
+++ streaming/views/ffmpeg.sh
@@ -24,13 +24,12 @@
 # mkdir 720;
 path=$1;
 cd ./views/uploads/$path;
-rm *.m4s;
 ffmpeg -i $path \
   -map 0:v:0 -map 0:a\?:0 -map 0:v:0 -map 0:a\?:0 -map 0:v:0 -map 0:a\?:0 \
   -b:v:0 250k  -filter:v:0 "scale=-2:240" -profile:v:0 baseline \
   -b:v:1 750k  -filter:v:1 "scale=-2:480" -profile:v:1 main \
   -b:v:2 1500k -filter:v:2 "scale=-2:720" -profile:v:2 high \
-  -b:v:3 5000k -filter:v:3 "scale=-2:1080" -profile:v:3 high10 \
+  -b:v:3 5000k -filter:v:2 "scale=-2:1080" -profile:v:3 high10 \
   -use_timeline 1 -use_template 1 -segment_time 2 -adaptation_sets "id=0,streams=v  id=1,streams=a" \
   -f dash output.mpd
 
