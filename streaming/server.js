ef59f0d21c87bf1ce5e22e776a8328fe00caf57a
FELLAH-tech <hicham8fellah@gmail.com>
2022-12-08 13:08:55 +0100

    auth

--- streaming/server.js
+++ streaming/server.js
@@ -7,14 +7,118 @@ const busboy = require('busboy');
 var sys = require('sys')
 var exec = require('child_process').exec;
 
+
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('Data_Base.db');
+
+const {openDb} = require("./db");
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+
+const session = require('express-session');
+//app.set('trust proxy', 1)
+app.use(session(
+    {
+        secret: 'secret key',
+        resave: true,
+        rolling: true,
+        saveUninitialized: true,
+        cookie: { 
+            maxAge: 1000 * 3600 //ms    
+        },
+        saveUninitialized: true
+    }
+))
+
+
 app.set('view engine', 'pug');
 
 app.use(express.static(path.join(__dirname, 'views')))
 const upload = multer({ dest: "views/uploads/" });
 
 
-app.get('/', function(req, res) {
-  res.render('index', { path: '', name: 'uploading... !' });
+
+
+app.get('/', async (req, res)=>{
+  if(!req.session.pseudo){
+      res.redirect('/authen')
+  }else{
+      let db_select = await openDb();
+     
+      let user = {
+          id : req.session.user_id,
+          pseudo : req.session.pseudo
+      }
+      let data = {
+          user : user,
+          tag : req.session.tag
+      }
+      res.render("index.pug", { user: user.pseudo});
+  }
+});
+
+
+
+
+app.get('/inscription', (req, res)=>{
+
+  res.render('inscription.pug');
+
+});
+
+app.post('/inscription', (req, res)=>{
+  // Checking info & saving data
+  db.run(`
+      INSERT INTO Users(pseudo, email, password)
+      VALUES
+          (?, ?, ?);
+  `, req.body.pseudo, req.body.email, req.body.password);
+  res.redirect('/');
+});
+
+app.get('/authen', (req, res)=>{
+
+  data = {
+      err_msg : ""
+  }
+  res.render('authen.pug', data);
+
+});
+
+app.post('/authen', (req, res)=>{
+  // Checking info & saving data
+  err_msg = "2892";
+  err = false;
+  db.get("SELECT * FROM Users WHERE email = ?", [req.body.email],
+  (err, row)=>{
+      if(typeof row === 'undefined'){
+          err_msg = "There is no user with this email";
+          err = true;
+      }else if(row.password != req.body.password){
+          err_msg = "Wrong password";
+          err = true;
+      }
+
+      if(err){
+          data = {
+              err_msg : err_msg
+          }
+          res.render('authen.pug', data);
+      }else{
+          req.session.user_id = row.id
+          req.session.pseudo = row.pseudo
+          req.session.email = row.email
+          res.redirect('/');
+      }
+  });
+});
+
+app.get('/deconnect', (req, res)=>{
+  req.session.destroy()
+  res.redirect('/')
 })
 
 app.post('/upload', function(req, res) {
